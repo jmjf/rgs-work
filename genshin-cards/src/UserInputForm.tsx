d45a70414c27cf0227f65428113e95a59baae0f9
jmjf <jamee.mikell@gmail.com>
2022-06-11 02:08:03 +0000

    2.3.5 - REFACTOR: replace reference with controlled component

--- genshin-cards/src/UserInputForm.tsx
+++ genshin-cards/src/UserInputForm.tsx
@@ -1,21 +1,23 @@
-import React, { RefObject } from 'react';
+import React from 'react';
 import './UserInputForm.css';
 
 export class UserInputForm extends React.Component {
-   nameInput = React.createRef() as RefObject<HTMLInputElement>;
+   state = {
+      name: ''
+   };
 
    handleSubmit = (ev: React.SyntheticEvent) => {
       ev.preventDefault();
-      const name = this.nameInput.current?.value;
-      console.log(name);
+      console.log(this.state.name);
    }
 
    render() {
       return (
          <form onSubmit={this.handleSubmit}>
             <input 
-               ref={this.nameInput}
                type="text"
+               value={this.state.name}
+               onChange={event => this.setState({ name: event.target.value })}
                placeholder="Character name"
                required
             />
